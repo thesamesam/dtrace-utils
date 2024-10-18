b3c659ef7ffda475a4c18ecb3917b11ae691741d
Eugene Loh <eugene.loh@oracle.com>
2024-10-18 16:51:28 -0700

    test: Remove misformatted and incorrect @@xfail
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/begin/tst.begin-starvation.sh
+++ test/unittest/begin/tst.begin-starvation.sh
@@ -5,8 +5,6 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
-# @@ xfail: BEGIN not yet forced to be processed first
-#
 
 #
 # This script tests that BEGIN in conjunction with a high-volume probe
