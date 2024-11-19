51e4cd22da9902c0ed047087ed39e640d341b97e
Eugene Loh <eugene.loh@oracle.com>
2024-11-19 10:21:52 -0500

    test: Number refers to nonexistent line
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- test/unittest/misc/err.bad-prov.r
+++ test/unittest/misc/err.bad-prov.r
@@ -1,2 +1,2 @@
 -- @@stderr --
-dtrace: failed to compile script test/unittest/misc/err.bad-prov.d: line 20: syntax error near "}"
+dtrace: failed to compile script test/unittest/misc/err.bad-prov.d: line 16: syntax error near "}"
